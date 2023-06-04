7e4f0873d6870485fc1de47665af6b44b15499d9
Virux <starletgh@gmail.com>
2023-06-04 15:00:22 +0000

    keywords

---
+++ dart/keywords.dart
@@ -0,0 +1,115 @@
+// ol' Good Keywords
+// words treated specially in a prog lang
+
+// * - contextual keywords
+// ** - limited(reserved for asynchrony support)
+// ^ - built-in identifiers
+
+// A
+abstract ^
+as ^
+assert
+async *
+await **
+
+// B
+base ^
+break
+
+
+// C
+case
+catch
+class
+const
+continue
+covariant ^
+
+
+// D
+default ^
+deferred ^
+do
+dynamic ^
+
+// E
+else 
+enum
+export ^
+extends
+extension ^
+external ^
+
+// F
+factory ^
+false
+final (variable)
+final (class) ^
+finally
+for
+Function ^
+get ^
+hide *
+
+// I
+if
+implements ^
+import ^
+in
+interface
+is
+
+
+// L
+late ^
+library ^
+
+// M
+mixin ^
+
+// N
+new
+null
+
+// O
+on *
+operator ^
+
+// P
+part ^
+
+// R
+required ^
+rethrow 
+return
+
+// S
+sealed
+set ^
+show *
+static ^
+super
+switch 
+sync *
+
+// T
+this
+throw
+true
+try
+typedef ^
+
+// V
+var
+void
+
+// W
+when
+while
+with
+
+// Y
+yield **
+
+
+
