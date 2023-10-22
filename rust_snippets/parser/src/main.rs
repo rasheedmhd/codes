aa1f4212fc1498059a34a0d11d1860bdbd831d28
Virux <starletgh@gmail.com>
2023-10-22 10:26:19 +0000

    building a parser using parser combinators

---
+++ rust_snippets/parser/src/main.rs
@@ -0,0 +1,5 @@
+fn main() {
+    println!("Hello, world!");
+    println!("Let's build a parser, shall we?");
+    println!("yay!");
+}
---
+++ rust_snippets/parser/target/debug/parser.d
@@ -0,0 +1,1 @@
+/Users/Starlet/codes/rust_snippets/parser/target/debug/parser: /Users/Starlet/codes/rust_snippets/parser/src/main.rs
