eab40c1f4317767fb296d3cd9f9f9ead0329adce
Virux <starletgh@gmail.com>
2023-11-07 10:05:04 +0000

    bla

---
+++ rust_snippets/parser/target/debug/deps/parser-0f2273c302e69102.d
@@ -0,0 +1,5 @@
+/Users/Starlet/codes/rust_snippets/parser/target/debug/deps/parser-0f2273c302e69102.rmeta: src/lib.rs
+
+/Users/Starlet/codes/rust_snippets/parser/target/debug/deps/parser-0f2273c302e69102.d: src/lib.rs
+
+src/lib.rs:
