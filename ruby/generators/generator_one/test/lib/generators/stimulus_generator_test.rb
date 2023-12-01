2a7dfc8b29c5c4569d4b3e9dd216392df4276ee9
rasheedmhd <starletgh@gmail.com>
2023-12-01 11:10:04 +0000

    test passing

---
+++ ruby/generators/generator_one/test/lib/generators/stimulus_generator_test.rb
@@ -0,0 +1,20 @@
+require "test_helper"
+require "generators/stimulus/stimulus_generator"
+
+class StimulusGeneratorTest < Rails::Generators::TestCase
+  tests StimulusGenerator
+  destination Rails.root.join("tmp/generators")
+  setup :prepare_destination
+
+  # test "generator runs without errors" do
+  #   assert_nothing_raised do
+  #     run_generator ["arguments"]
+  #   end
+  # end
+
+  test 'should add thing controller file' do
+    run_generator ['Thing']
+
+    assert_file 'app/javascript/controllers/thing_controller.js'
+  end
+end
