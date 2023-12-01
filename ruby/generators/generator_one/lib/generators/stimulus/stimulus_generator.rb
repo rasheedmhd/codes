3a24d0820b4a7cf6f5279ef2f0e04396c287baf8
rasheedmhd <starletgh@gmail.com>
2023-12-01 11:49:14 +0000

    honeybadger build a rails generator- conclusion

--- ruby/generators/generator_one/lib/generators/stimulus/stimulus_generator.rb
+++ ruby/generators/generator_one/lib/generators/stimulus/stimulus_generator.rb
@@ -1,6 +1,9 @@
 class StimulusGenerator < Rails::Generators::NamedBase
   source_root File.expand_path("templates", __dir__)
 
+  # When taking arguments in our generators, we can use the class_option method.
+  class_option :actions, type: :array, default: []
+
   def create_stimulus_controller
     # create_file "app/javascript/controllers/#{file_path}_controller.js"
     template 'stimulus_controller.js.erb', "app/javascript/controllers/#{file_path}_controller.js"
--- ruby/generators/generator_one/test/lib/generators/stimulus_generator_test.rb
+++ ruby/generators/generator_one/test/lib/generators/stimulus_generator_test.rb
@@ -26,4 +26,13 @@ class StimulusGeneratorTest < Rails::Generators::TestCase
      assert_match /@hotwired/, content
     end
    end
+
+  test 'should generate actions' do
+  run_generator ["toggle", "--actions", "toggle hide"]
+  assert_file "app/javascript/controllers/toggle_controller.js" do |content|
+    assert_match /toggle()/, content
+    assert_match /hide()/, content
+  end
+  end
+
 end
