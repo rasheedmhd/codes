a37c51e43a12657abf88d3ec01ce8b544592e7af
rasheedmhd <starletgh@gmail.com>
2024-04-19 00:20:17 +0000

    rust OsStr

--- strings/rstr.rs
+++ strings/rstr.rs
@@ -19,6 +19,8 @@
 //
 //
 
+// In Rust, provided you don't explicitly work around it with unsafe, values of type String are always valid UTF-8.
+
 // Now we can gracefully handle the error
 fn main() {
     let arg = std::env::args_os()
@@ -31,3 +33,29 @@ fn main() {
         None => println!("not valid UTF-8: {:?}", arg),
     }
 }
+
+// What if we want to print the Unicode scalar values's numbers instead of their, uh, graphemes?
+// fn main() {
+//     let arg = std::env::args()
+//         .skip(1)
+//         .next()
+//         .expect("should have one argument");
+
+//     for c in arg.chars() {
+//         print!("{} (U+{:04X}) ", c, c as u32);
+//     }
+//     println!()
+// }
+
+// What if we want to show how it's encoded as UTF-8? By which I mean, print the individual bytes?
+// fn main() {
+//     let arg = std::env::args()
+//         .skip(1)
+//         .next()
+//         .expect("should have one argument");
+
+//     for b in arg.bytes() {
+//         print!("{:02X} ", b);
+//     }
+//     println!()
+// }
