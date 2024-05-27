510556a2a97842ccf74953ecee50b112ea4fd0a1
rasheedmhd <starletgh@gmail.com>
2024-05-27 10:35:11 +0000

    running nginx on docker locally with tf

--- DevOps/test/output.tf
+++ DevOps/test/output.tf
@@ -1,12 +1,12 @@
-output "instance_id" {
-  value       = aws_instance.test_server.id
-  sensitive   = true
-  description = "An EC2 instance Id"
-  depends_on  = []
-}
-output "instance_public_ip" {
-  value       = aws_instance.test_server.public_ip
-  sensitive   = true
-  description = "An EC2 instance public internet address"
-  depends_on  = []
-}
+# output "instance_id" {
+#   value       = aws_instance.test_server.id
+#   sensitive   = true
+#   description = "An EC2 instance Id"
+#   depends_on  = []
+# }
+# output "instance_public_ip" {
+#   value       = aws_instance.test_server.public_ip
+#   sensitive   = true
+#   description = "An EC2 instance public internet address"
+#   depends_on  = []
+# }
