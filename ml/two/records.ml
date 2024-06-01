6eada5327221be43bf16b4e1276399d0c6f74730
rasheedmhd <starletgh@gmail.com>
2024-06-01 23:23:32 +0000

    records

--- ml/two/records.ml
+++ ml/two/records.ml
@@ -76,3 +76,67 @@ lifetime henryV ;
 lifetime richardIII ;
 
 fun lifetime({name,born,crowned,died,quote}) = died - born;
+
+(* INFIX OPERATORS *)
+(* We take infix notation for granted in mathematics. 
+Imagine doing without it. Instead of 2+2=4 we should have to write =(+(2,2),4). 
+Most functional languages let programmers declare their own infix operators. *)
+
+infix xor;
+
+fun (p xor q) = (p orelse q) andalso not (p andalso q);
+
+(* An ML infix directive may state a precedence from 0 to 9. 
+default precedence = 0 = lowest. 
+infix is left associated, while infixr is right associated. *)
+infix 6 plus;
+fun (a plus b) = "(" ^ a ^ "+" ^ b ^ ")";
+"1" plus "2" plus "3";
+
+infix 7 times;
+fun (a times b) = "(" ^ a ^ "*" ^ b ^ ")";
+"m" times "n" times "3" plus "i" plus "j" times "k";
+
+(* right associated *)
+(* It produces a # sign. (ML has no operator for powers.) *)
+infix 8 pow;
+fun (a pow b) = "(" ^ a ^ "#" ^ b ^ ")";
+"m" times "i" pow "j" pow "2" times "n";
+
+(* Many infix operators have symbolic names. 
+Let ++ be the operator for vector addition: *)
+infix ++;
+fun ((x1,y1) ++ (x2,y2)) : vec = (x1+x2, y1+y2);
+
+(* Keep symbolic names separate. 
+Symbolic names can cause confusion if you run them together. 
+Below, ML reads the characters +~ as one symbolic name,
+then complains that this name has no value: *)
+
+1+~3;
+(* > Unknown name +~ *)
+(* Symbolic names must be separated by spaces or other characters: *)
+1+ ~3;
+
+(* Taking infixes as functions. Occasionally an infix has to be treated like an ordinary function. 
+In ML the keyword op overrides infix status: if ⊕ is an infix
+  operator then op⊕ is the corresponding function, which can be applied to a pair
+  in the usual way. *)
+op++ ((2.5,0.0), (0.1,2.5));
+
+(* Infix status can be revoked. If ⊕ is an infix operator then the directive nonfix⊕
+makes it revert to ordinary function notation. 
+A subsequent infix directive can make ⊕ an infix operator again.
+Here we deprive ML's multiplication operator of its infix status. 
+The attempt to use it produces an error message, since we may not apply 3 as a function. 
+But * can be applied as a function: *)
+
+nonfix *;
+3*2;
+(* > Error: Type conflict... *)
+*(3,2);
+> 6 : int
+(* The nonfix directive is intended for interactive development of syntax, for
+trying different precedences and association. Changing the infix status of established operators leads to madness. *)
+
+(* You hear that? Pure madness, insanity *)
