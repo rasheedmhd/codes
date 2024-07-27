7b355c88e7cc8518c901559ab36dc54ccb6ea890
rasheedmhd <starletgh@gmail.com>
2024-07-27 23:09:58 +0000

    malloc

--- sys/ch2/malloc.c
+++ sys/ch2/malloc.c
@@ -25,6 +25,33 @@ int main() {
     // When a program no longer needs the heap memory it dynamically
     // al­located with malloc, it should explicitly deallocate the
     // memory by calling the free function
+
+
+
+    // Dynamically Allocated Arrays and Strings
+    int *arr;
+    char *c_arr;
+    // allocate an array of 20 ints on the heap:
+    arr = malloc(sizeof(int) * 20);
+    // allocate an array of 10 chars on the heap:
+    c_arr = malloc(sizeof(char) * 10);
+
+
     free(p);
     p = NULL;
+
+    int i;
+    int s_array[20];
+    int *d_array;
+    d_array = malloc(sizeof(int) * 20);
+    if (d_array == NULL) {
+        printf("Error: malloc failed\n");
+        exit(1);
+    }
+    for (i=0; i < 20; i++) {
+        s_array[i] = i;
+        d_array[i] = i;
+    }
+    printf("%d %d \n", s_array[3], d_array[3]);  // prints 3 3
+
 }
