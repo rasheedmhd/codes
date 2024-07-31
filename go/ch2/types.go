f265dbf7fcfc869898bed4da49d59f407cf99bc5
rasheedmhd <starletgh@gmail.com>
2024-07-31 23:23:40 +0000

    go types

--- go/ch2/types.go
+++ go/ch2/types.go
@@ -4,8 +4,17 @@ package main
 
 import "fmt"
 
-func main() {
+// Immutable variables
+const cx int64 = 10
 
+const (
+	idKey   = "id"
+	nameKey = "name"
+)
+const z = 20 * 10
+
+func main() {
+ 
 	var x int = 10
 	var y float64 = 30.2
 	var sum1 float64 = float64(x) + y
@@ -18,4 +27,33 @@ func main() {
 	var sum4 byte = byte(x) + b
 	fmt.Println(sum3, sum4)
 
+	// Go doesn’t provide a way to specify that a value calculated at runtime is immutable
+	x2 := 5
+	y2 := 10
+	const z = x2 + y2
+
+	const cy = "hello"
+	fmt.Println(cx)
+	fmt.Println(cy)
+	cx = cx + 1 // this will not compile!
+	cy = "bye"  // this will not compile!
+	
+	
+	// Constants can be typed or untyped
+	
+	// Go doesn't allow unused declared variables
+	// Go requirement is that every declared local variable must be read.
+	// It is a compile-time error to declare a local variable and to not read its value.
+
 }
+
+// Constants in Go are a way to give names to literals.
+// They can only hold values that the compiler can figure out at compile time.
+//
+// This means that they can be assigned:
+// • Numeric literals
+// • true and false
+// • Strings
+// • Runes
+// • The values returned by the built-in functions complex, real, imag, len, and cap
+// • Expressions that consist of operators and the preceding values
