3a8e9f26c000902f155bc3e1960e98a358864707
rasheedmhd <starletgh@gmail.com>
2024-08-03 00:33:04 +0000

    1.2 Cmd Line Args

--- go/tgpl/echo.go
+++ go/tgpl/echo.go
@@ -8,12 +8,19 @@ import (
 
 func main() {
 	var s, sep string
-	for i := 1; i < len(os.Args); i++ {
+	for i := 0; i < len(os.Args); i++ {
 		s += sep + os.Args[i]
 		sep = " "
 	}
 	fmt.Println(s)
 }
+
+// Exercise 1
+// func main() {
+// 	for i, arg := range os.Args[1:] {
+// 		fmt.Println(i, arg)
+// 	}
+// }
 
 // other forms of Go for
 // for condition {} -> traditional while loop
