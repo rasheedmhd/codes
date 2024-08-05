f45e6497804c1f758ae73992ccac06aeddcfef84
rasheedmhd <starletgh@gmail.com>
2024-08-05 00:18:13 +0000

    Dim3 - dup3 improved

--- go/tgpl/ch1/dup3.go
+++ go/tgpl/ch1/dup3.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 	"strings"
 )
@@ -10,16 +9,21 @@ import (
 func main() {
 
 	counts := make(map[string]int)
+
 	for _, filename := range os.Args[1:] {
-		data, err := ioutil.ReadFile(filename)
+		data, err := os.ReadFile(filename)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
 			continue
 		}
 		for _, line := range strings.Split(string(data), "\n") {
+			if line == "" {
+				continue
+			}
 			counts[line]++
 		}
 	}
+
 	for line, n := range counts {
 		if n > 1 {
 			fmt.Printf("%d\t%s\n", n, line)
