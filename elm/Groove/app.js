c6d79c6ec63462834c9293c791a4a5be17a13f04
rasheedmhd <starletgh@gmail.com>
2024-08-07 02:13:17 +0000

    partial application: not working yet !

--- elm/Groove/app.js
+++ elm/Groove/app.js
@@ -4419,6 +4419,12 @@ var $elm$virtual_dom$VirtualDom$toHandlerInt = function (handler) {
 			return 3;
 	}
 };
+var $author$project$PhotoGroove$initialModel = _List_fromArray(
+	[
+		{url: '1.jpeg'},
+		{url: '2.jpeg'},
+		{url: '3.jpeg'}
+	]);
 var $elm$json$Json$Encode$string = _Json_wrap;
 var $elm$html$Html$Attributes$stringProperty = F2(
 	function (key, string) {
@@ -4428,9 +4434,6 @@ var $elm$html$Html$Attributes$stringProperty = F2(
 			$elm$json$Json$Encode$string(string));
 	});
 var $elm$html$Html$Attributes$class = $elm$html$Html$Attributes$stringProperty('className');
-var $elm$html$Html$div = _VirtualDom_node('div');
-var $elm$html$Html$h1 = _VirtualDom_node('h1');
-var $elm$html$Html$Attributes$id = $elm$html$Html$Attributes$stringProperty('id');
 var $elm$html$Html$img = _VirtualDom_node('img');
 var $elm$html$Html$Attributes$src = function (url) {
 	return A2(
@@ -4438,6 +4441,89 @@ var $elm$html$Html$Attributes$src = function (url) {
 		'src',
 		_VirtualDom_noJavaScriptOrHtmlUri(url));
 };
+var $author$project$PhotoGroove$urlprefix = 'http://elm-in-action.com/';
+var $author$project$PhotoGroove$createThumbnailUrl = function (thumbnail) {
+	return A2(
+		$elm$html$Html$img,
+		_List_fromArray(
+			[
+				$elm$html$Html$Attributes$src(
+				_Utils_ap($author$project$PhotoGroove$urlprefix, thumbnail.url))
+			]),
+		_List_Nil);
+};
+var $elm$html$Html$div = _VirtualDom_node('div');
+var $elm$html$Html$h1 = _VirtualDom_node('h1');
+var $elm$html$Html$Attributes$id = $elm$html$Html$Attributes$stringProperty('id');
+var $elm$core$List$foldrHelper = F4(
+	function (fn, acc, ctr, ls) {
+		if (!ls.b) {
+			return acc;
+		} else {
+			var a = ls.a;
+			var r1 = ls.b;
+			if (!r1.b) {
+				return A2(fn, a, acc);
+			} else {
+				var b = r1.a;
+				var r2 = r1.b;
+				if (!r2.b) {
+					return A2(
+						fn,
+						a,
+						A2(fn, b, acc));
+				} else {
+					var c = r2.a;
+					var r3 = r2.b;
+					if (!r3.b) {
+						return A2(
+							fn,
+							a,
+							A2(
+								fn,
+								b,
+								A2(fn, c, acc)));
+					} else {
+						var d = r3.a;
+						var r4 = r3.b;
+						var res = (ctr > 500) ? A3(
+							$elm$core$List$foldl,
+							fn,
+							acc,
+							$elm$core$List$reverse(r4)) : A4($elm$core$List$foldrHelper, fn, acc, ctr + 1, r4);
+						return A2(
+							fn,
+							a,
+							A2(
+								fn,
+								b,
+								A2(
+									fn,
+									c,
+									A2(fn, d, res))));
+					}
+				}
+			}
+		}
+	});
+var $elm$core$List$foldr = F3(
+	function (fn, acc, ls) {
+		return A4($elm$core$List$foldrHelper, fn, acc, 0, ls);
+	});
+var $elm$core$List$map = F2(
+	function (f, xs) {
+		return A3(
+			$elm$core$List$foldr,
+			F2(
+				function (x, acc) {
+					return A2(
+						$elm$core$List$cons,
+						f(x),
+						acc);
+				}),
+			_List_Nil,
+			xs);
+	});
 var $elm$virtual_dom$VirtualDom$text = _VirtualDom_text;
 var $elm$html$Html$text = $elm$virtual_dom$VirtualDom$text;
 var $author$project$PhotoGroove$view = function (model) {
@@ -4462,31 +4548,8 @@ var $author$project$PhotoGroove$view = function (model) {
 					[
 						$elm$html$Html$Attributes$id('thumbnails')
 					]),
-				_List_fromArray(
-					[
-						A2(
-						$elm$html$Html$img,
-						_List_fromArray(
-							[
-								$elm$html$Html$Attributes$src('http://elm-in-action.com/1.jpeg')
-							]),
-						_List_Nil),
-						A2(
-						$elm$html$Html$img,
-						_List_fromArray(
-							[
-								$elm$html$Html$Attributes$src('http://elm-in-action.com/2.jpeg')
-							]),
-						_List_Nil),
-						A2(
-						$elm$html$Html$img,
-						_List_fromArray(
-							[
-								$elm$html$Html$Attributes$src('http://elm-in-action.com/3.jpeg')
-							]),
-						_List_Nil)
-					]))
+				A2($elm$core$List$map, $author$project$PhotoGroove$createThumbnailUrl, model))
 			]));
 };
-var $author$project$PhotoGroove$main = $author$project$PhotoGroove$view('no model yet');
+var $author$project$PhotoGroove$main = $author$project$PhotoGroove$view($author$project$PhotoGroove$initialModel);
 _Platform_export({'PhotoGroove':{'init':_VirtualDom_init($author$project$PhotoGroove$main)(0)(0)}});}(this));
--- elm/Groove/src/PhotoGroove.elm
+++ elm/Groove/src/PhotoGroove.elm
@@ -18,31 +18,42 @@ import Html exposing (..)
 -- https://package.elm-lang.org/packages/elm/html/latest/Html-Attributes
 import Html.Attributes exposing (..)
 
-urlprefix =
+urlPrefix =
  "http://elm-in-action.com/"
 
 view model =
  div [ class "content" ]
   [ h1 [] [ text "Photo Groove" ]
-  , div [ id "thumbnails" ] (List.map viewThumbnail model)
+  , div [ id "thumbnails" ]
+    (List.map (createThumbnailUrl model.selectedUrl) model.photos)
+  , img
+    [ class "large"
+    , src (urlPrefix ++ "large/" ++ model.selectedUrl)
+    ]
+    []
   ]
- -- div [ class "content" ]
- --  [h1 [] [text "Photo Groove" ]
- --   , div [ id "thumbnails" ]
- --   [ img [ src "http://elm-in-action.com/1.jpeg" ] []
- --   , img [ src "http://elm-in-action.com/2.jpeg" ] []
- --   , img [ src "http://elm-in-action.com/3.jpeg" ] []
- --  ]
- -- ]
 
-viewThumbnail thumb =
- img [ src (urlprefix ++ thumb.url) ] []
+-- Helper/translation function
+createThumbnailUrl selectedUrl thumbnail =
+-- Return image but in a situation where by the selectedUrl is
+-- the same as the url of the thumbnail,
+-- there by returning true, add a class attribute
+-- with the name of the classList string to the image and return it
+ img
+  [ src (urlPrefix ++ thumbnail.url)
+  , classList [ ( "selected", selectedUrl == thumbnail.url ) ]
+  ]
+
 
 initialModel =
- [ { url = "1.jpeg" }
- , { url = "2.jpeg" }
- , { url = "3.jpeg" }
- ]
+ { photos =
+  [ { url = "1.jpeg" }
+  , { url = "2.jpeg" }
+  , { url = "3.jpeg" }
+  ]
+  , selectedUrl = "1.jpeg"
+ }
+
 
 main =
  view initialModel
