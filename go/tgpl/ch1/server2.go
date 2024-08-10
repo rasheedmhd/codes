5753bef6d16b913180133512041e0957e702841f
rasheedmhd <starletgh@gmail.com>
2024-08-10 23:58:51 +0000

    Server3

--- go/tgpl/ch1/server2.go
+++ go/tgpl/ch1/server2.go
@@ -21,13 +21,34 @@ func main() {
 }
 
 // handler echoes the Path component of the requested URL.
+// func handler(w http.ResponseWriter, r *http.Request) {
+
+// 	mu.Lock()
+// 	count++
+// 	mu.Unlock()
+// 	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+
+// }
+//
+// handler echoes the HTTP request.
 func handler(w http.ResponseWriter, r *http.Request) {
 
-	mu.Lock()
-	count++
-	mu.Unlock()
-	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
 
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
 }
 
 // counter echoes the number of calls so far.
