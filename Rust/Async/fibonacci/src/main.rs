7d69304a1d5037f5799981f8d2541c36eb418fbd
rasheedmhd <starletgh@gmail.com>
2024-08-17 12:28:42 +0000

    Learning Async Rust

---
+++ Rust/Async/fibonacci/src/main.rs
@@ -0,0 +1,23 @@
+use std::thread;
+
+fn main() {
+    let mut threads = Vec::new();
+
+    for i in 0..8 {
+        let handle = thread::spawn(move || {
+            let result = fibonacci(4000);
+            println!("THREAD {} RESULT: {}", i, result);
+        });
+        threads.push(handle);
+    }
+    for handle in threads {
+        handle.join().unwrap();
+    }
+}
+
+fn fibonacci(n: u64) -> u64 {
+    if n == 0 || n == 1 {
+        return n;
+    }
+    fibonacci(n - 1) + fibonacci(n - 2)
+}
---
+++ Rust/Async/fibonacci/target/debug/deps/fibonacci-1df05dc89faf1858.d
@@ -0,0 +1,5 @@
+/Users/Starlet/codes/Rust/Async/fibonacci/target/debug/deps/fibonacci-1df05dc89faf1858: src/main.rs
+
+/Users/Starlet/codes/Rust/Async/fibonacci/target/debug/deps/fibonacci-1df05dc89faf1858.d: src/main.rs
+
+src/main.rs:
