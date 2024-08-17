a819161bc76d11d2487a129b5546d0408cc31742
rasheedmhd <starletgh@gmail.com>
2024-08-17 13:11:39 +0000

    tokio with async

--- Rust/Async/fibonacci/src/main.rs
+++ Rust/Async/fibonacci/src/main.rs
@@ -30,11 +30,14 @@ async fn main() -> Result<(), Error> {
     let url = "https://jsonplaceholder.typicode.com/posts/1";
 
     let start_time = Instant::now();
-    let _ = reqwest::get(url).await?;
-    let _ = reqwest::get(url).await?;
-    let _ = reqwest::get(url).await?;
-    let _ = reqwest::get(url).await?;
-    let _ = reqwest::get(url).await?;
+
+    let (_, _, _, _, _) = tokio::join!(
+	    reqwest::get(url),
+	    reqwest::get(url),
+	    reqwest::get(url),
+	    reqwest::get(url),
+	    reqwest::get(url),
+    );
 
     let elapsed_time = start_time.elapsed();
     println!("Request took {} ms", elapsed_time.as_millis());
