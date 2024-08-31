bb7286297172d363b6ce9c868949f5947c894800
rasheedmhd <starletgh@gmail.com>
2024-08-31 23:59:34 +0000

    .

--- zero2prod/src/run.rs
+++ zero2prod/src/run.rs
@@ -1,14 +1,29 @@
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
 use std::net::TcpListener;
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+async fn subscribe(form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 pub fn start(tcp_listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(tcp_listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(tcp_listener)?
+    .run();
 
     Ok(server)
 }
