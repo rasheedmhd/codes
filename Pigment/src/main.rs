d25d147e44b1e4c241d828cb003fb5532a0f07a7
rasheedmhd <starletgh@gmail.com>
2024-09-02 23:59:23 +0000

    .

--- Pigment/src/main.rs
+++ Pigment/src/main.rs
@@ -1,10 +1,14 @@
 use std::net::TcpListener;
+use sqlx::PgConnection;
 use pigment::startup::run;
 use pigment::configuration::get_configuration;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     let tcp_listener = TcpListener::bind("127.0.0.1:0")?;
-    run(tcp_listener)?.await
+    run(tcp_listener, connection)?.await
 }
--- Pigment/src/startup.rs
+++ Pigment/src/startup.rs
@@ -3,11 +3,18 @@ use crate::routes::subscriptions::subscribe;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
 use std::net::TcpListener;
-pub fn run(tcp_listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+use sqlx::PgConnection;
+
+pub fn run(tcp_listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+    // Wrap the connection in a smart pointer
+    let connection = web::Data::new(connection);
+    // Capture `connection` from the surrounding environment
+    let server = HttpServer::new(move|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            // Register the connection as part of the application state
+            .app_data(connection.clone())
     })
     .listen(tcp_listener)?
     .run();
