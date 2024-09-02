b1ea4e4a9fa059ebdd10f9587de1f34644e472d0
rasheedmhd <starletgh@gmail.com>
2024-09-02 23:44:46 +0000

    Using a .env file to give sqlx a db url to test again at comp-time

--- Pigment/tests/health_check.rs
+++ Pigment/tests/health_check.rs
@@ -36,7 +36,7 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     let connection_string = configuration.database.connection_string();
     // The `Connection` trait MUST be in scope before we can invoke
     // `PgConnection::connect`
-    let connection = PgConnection::connect(&connection_string)
+    let mut connection = PgConnection::connect(&connection_string)
         .await
         .expect("Failed to connect to Postgres.");
     let client = reqwest::Client::new();
@@ -50,8 +50,23 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .await
         .expect("Failed to execute request.");
 
+    // Assert
     assert_eq!(200, response.status().as_u16());
+
+    // The biggest downside to query!() is that the output type cannot be named
+    // (due to Rust not officially supporting anonymous records).
+    // To address that, there is a query_as!() macro that is mostly
+    // identical except that you can name the output type.
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
+
+
 #[tokio::test]
 async fn subscribe_returns_a_400_when_data_is_missing() {
     let app_address = spawn_app();
