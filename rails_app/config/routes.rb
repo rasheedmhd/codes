89428d5ca7335a9b0d0340051a9356ef080dbc11
rasheedmhd <starletgh@gmail.com>
2024-09-15 16:10:15 +0000

    Rendering and investigating scaffolds

--- rails_app/config/routes.rb
+++ rails_app/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :products
   get 'home/index'
 
   root "home#index"
--- rails_app/db/schema.rb
+++ rails_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_14_130354) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_15_133427) do
   create_table "candidates", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -30,6 +30,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_14_130354) do
     t.string "title"
     t.string "poster"
     t.string "company"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "image_url"
+    t.decimal "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
