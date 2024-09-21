397ffe289aa33d9048b6fab347fbb9e50144c659
rasheedmhd <starletgh@gmail.com>
2024-09-21 16:38:37 +0000

    rendering html

--- dispatch_me/app/controllers/demo_controller.rb
+++ dispatch_me/app/controllers/demo_controller.rb
@@ -1,4 +1,7 @@
 class DemoController < ApplicationController
   # Rails still understands and works with this, 
   # displaying the index file from app/views/demo
+  def renderrr
+    render html: "<strong>Not Found</strong>".html_safe
+  end
 end
