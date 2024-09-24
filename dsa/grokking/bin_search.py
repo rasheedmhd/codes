a4a19e5250dcc1ecb4797217a953d40d2a9f3ba0
rasheedmhd <starletgh@gmail.com>
2024-09-24 18:12:45 +0000

    binary search in python

---
+++ dsa/grokking/bin_search.py
@@ -0,0 +1,18 @@
+def binary_search(arr, item):
+    low = 0
+    high = len(arr) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        guess = arr[mid]
+        if guess == item:
+            return mid 
+        elif guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+d
+my_list = [1,3,5,7,9]
+print(binary_search(my_list, 9)) # => 1
+print(binary_search(my_list, -1)) # => None
