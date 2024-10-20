641f891a85394193a0432f0e93a2d58a7a9f211a
rasheedmhd <starletgh@gmail.com>
2024-10-20 16:58:56 +0000

    Type hints allows tools like mypy to do static type checking

--- fast/type_hints.py
+++ fast/type_hints.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 def get_full_name(first_name: str, last_name: str):
     # Type hints allow the code environment to offer method suggestions
     # based on what the type of the value is
@@ -15,4 +17,29 @@ def get_name_with_age(name: str, age: int):
     name_with_age = name + " is " + str(age) + " years old."
     return name_with_age
 
-print(get_name_with_age("Liz", 45))
+# print(get_name_with_age("Liz", 45))
+
+# What Optional Really means 
+# TypeError: say_hi() missing 1 required positional argument: 'name'
+def say_hi(name: Optional[str]):
+# You must provide a default value for when a value is not provider
+# So Optional is not really Optional.
+# It is more like an Enum, that has only two variants, 
+# a str or a None(Or some other value)
+# def say_hi(name: Optional[str] = "Default"):
+# def say_hi(name: Optional[str] = 54):
+    print(f"Hey {name}!")
+
+# say_hi()
+
+# Or try an improved version with Python 3.10+
+# No more, Unions or Optional 
+def salut(name: str | None):
+    print(f"Salut! {name}")
+
+# salut(name="jay")
+
+def say_hi2(name: str | None):
+    print(f"Hey {name}!")
+
+say_hi2()
