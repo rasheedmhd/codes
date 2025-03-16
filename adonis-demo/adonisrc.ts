d3c7bb4cbe92bf9e503766efe604067a8dc7b655
rasheedmhd <starletgh@gmail.com>
2025-03-16 22:45:36 +0000

    URL conf + adonis

--- adonis-demo/adonisrc.ts
+++ adonis-demo/adonisrc.ts
@@ -32,6 +32,7 @@ export default defineConfig({
     () => import('@adonisjs/cors/cors_provider'),
     () => import('@adonisjs/session/session_provider'),
     () => import('@adonisjs/lucid/database_provider'),
+    () => import('@adonisjs/static/static_provider'),
   ],
 
   /*
@@ -68,4 +69,10 @@ export default defineConfig({
     ],
     forceExit: false,
   },
+  metaFiles: [
+    {
+      pattern: 'public/**',
+      reloadServer: false,
+    },
+  ],
 })
