fdddad1ef772b439b0c74f9eeb1a0d0b7c2167e3
rasheedmhd <starletgh@gmail.com>
2025-07-01 23:04:29 +0000

    Recon

--- hack_notes/Black Hat GraphQL/instropection.sh
+++ hack_notes/Black Hat GraphQL/instropection.sh
@@ -19,3 +19,100 @@ echo "Deleting empty introspections"
 find introspections -type f -empty -delete
 
 echo "Got $(find introspections -type f | wc -l) introspections from $total urls"
+
+# query IntrospectionQuery {
+#   __schema {
+#     queryType {
+#       name
+#     }
+#     mutationType {
+#       name
+#     }
+#     subscriptionType {
+#       name
+#     }
+#     types {
+#       ...FullType
+#     }
+#     directives {
+#       name
+#       description
+#       locations
+#       args {
+#         ...InputValue
+#       }
+#     }
+#   }
+# }
+# fragment FullType on __Type {
+#   kind
+#   name
+#   description
+#   fields(includeDeprecated: true) {
+#     name
+#     description
+#     args {
+#       ...InputValue
+#     }
+#     type {
+#       ...TypeRef
+#     }
+#     isDeprecated
+#     deprecationReason
+#   }
+#   inputFields {
+#     ...InputValue
+#   }
+#   interfaces {
+#     ...TypeRef
+#   }
+#   enumValues(includeDeprecated: true) {
+#     name
+#     description
+#     isDeprecated
+#     deprecationReason
+#   }
+#   possibleTypes {
+#     ...TypeRef
+#   }
+# }
+# fragment InputValue on __InputValue {
+#   name
+#   description
+#   type {
+#     ...TypeRef
+#   }
+#   defaultValue
+# }
+# fragment TypeRef on __Type {
+#   kind
+#   name
+#   ofType {
+#     kind
+#     name
+#     ofType {
+#       kind
+#       name
+#       ofType {
+#         kind
+#         name
+#         ofType {
+#           kind
+#           name
+#           ofType {
+#             kind
+#             name
+#             ofType {
+#               kind
+#               name
+#               ofType {
+#                 kind
+#                 name
+#               }
+#             }
+#           }
+#         }
+#       }
+#     }
+#   }
+# }
